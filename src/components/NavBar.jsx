bc765d24a9d3fae92d9f63a55677e9d5b01ebcdc
exede <dessolem@yahoo.com>
2021-12-21 23:48:03 -0300

    e-commerce

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Food and Beer</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Pizzas</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Empanadas</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Tartas</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Bebidas</a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                  <li><a className="dropdown-item" href="#">Cervezas</a></li>
+                  <li><a className="dropdown-item" href="#">Sin Alcohol</a></li>
+                  
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+}
+
+export default NavBar

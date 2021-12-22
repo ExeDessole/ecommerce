425539dd4c06630f63ed383fc96594c962a34cd6
exede <dessolem@yahoo.com>
2021-12-21 23:48:03 -0300

    e-commerce

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ItemListContainer() {
+    return (
+        <div>
+            <h1>Soy el componente</h1>
+        </div>
+    )
+}
+
+export default ItemListContainer
